05ca0333cccf2bcf3a38a0f6d78cb004216368ea
Daniel <obando.jd@gmail.com>
2019-04-04 22:31:44 +1100

    Variable de Entorno MONGO_URI Corregida url

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    ur = process.env.MONGO_URI;
+
+    urlDB = process.env.MONGO_URI;
+
     // MONGO_URI CREADA EN HEROKU.COM SITE COMO VARIABLE / IMPOSIBLE POR LINEA DE COMANDOS 
     // urlDB = 'mongodb://JDOP:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/cafe?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
 
