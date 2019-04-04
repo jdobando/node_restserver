f13aef3bbe5d20fae15a1efd3b8ce6eb0a32d8b5
Daniel <obando.jd@gmail.com>
2019-04-04 15:12:01 +1100

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,22 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // BASE DE DATOS'
 
 let urlDB;
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = 'mongodb://userDB:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/cafe?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
-}
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb://JDOP:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
 
+// }
 process.env.URLDB = urlDB;
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://JDOP:<password>@cluster0-xy0mg.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+//mongodb://userDB:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(require('./routes/usuario'));
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
     // mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    if (err) throw new err; // SI ERROR TERMINA AQUI.
+    if (err) throw err; // SI ERROR TERMINA AQUI.
     console.log('Base de Datos ONLINE'); // SI ES EXITOSO ESTA LINEA SE IMPRIME
 
 });
@@ -28,3 +28,15 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: tru
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto :', process.env.PORT);
 })
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://JDOP:<password>@cluster0-xy0mg.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+//mongodb://userDB:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin
