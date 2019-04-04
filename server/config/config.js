7c25ce8614f264d4cb6af8c88f5ee03bb61e228d
Daniel <obando.jd@gmail.com>
2019-04-04 15:19:04 +1100

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -10,12 +10,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // BASE DE DATOS'
 
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb://JDOP:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://JDOP:123456abcde@cluster0-shard-00-00-xy0mg.mongodb.net:27017,cluster0-shard-00-01-xy0mg.mongodb.net:27017,cluster0-shard-00-02-xy0mg.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
 
-// }
+}
 process.env.URLDB = urlDB;
 
 
